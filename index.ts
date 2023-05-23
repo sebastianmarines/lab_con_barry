0b7a3b62722f47bf3826bd33fa54f8f18226a739
Sebastian Marines <a01383056@tec.mx>
2023-05-23 09:14:33 -0600

    Connect DB

--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
+import knex from './db/connect';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+
+interface User {
+  id: number;
+  nombre: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  email: string;
+  password: string;
+}
+
+async function getLastUser(): Promise<User> {
+  return knex('usuario')
+    .select('*')
+    .orderBy('id', 'desc')
+    .first();
+}
+
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get('/', async (req: Request, res: Response) => {
+  console.log('GET /');
+  const user = await getLastUser();
+
+  res.send(user);
 });
 
 app.listen(port, () => {
