19e0b63b896b9b45a3aa3e96b5b4d70d3bd1f692
Sebastian Marines <a01383056@tec.mx>
2023-06-13 00:37:18 -0600

    Fix CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
